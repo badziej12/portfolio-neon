ef185cbdee9d1ef2829ec70f24dc4eb2e53a9bb7
badziej12 <badziej12@gmail.com>
2025-02-04 15:27:25 +0100

    Start screen
    Changes made:
     - Implemented start screen and gameboy animation
     - Implemented Gameboy component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import "@/styles/globals.css";
+import "@/styles/main.scss";
+import "@/styles/component/gameboy-component.scss";
 import type {AppProps} from "next/app";
 
 export default function App({Component, pageProps}: AppProps) {
