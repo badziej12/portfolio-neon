77360ca2508bf6de6fd15eb9f14e8d4cccf9493f
badziej12 <badziej12@gmail.com>
2025-02-05 15:52:53 +0100

    Implement ScreenSwitcher
    Changes made:
     - Implemented ScreenSwitcher witch allows user to switch between section in gameboy

--- src/components/GameboyScreen.tsx
+++ src/components/GameboyScreen.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import { MainMenu } from "./MainMenu";
+import { ScreenSwitcher } from "./ScreenSwitcher";
 
 export const GameboyScreen = () => {
+    const [currentScreen, setCurrentScreen] = useState("main-menu");
 
+    const handleScreenChange = (screen: string) => {
+        setCurrentScreen(screen);
+        console.log(screen);
+    }
 
     return (
         <div className={"gameboy-screen"}>
             <div className="gameboy-screen__content">
-                <MainMenu />
+                <ScreenSwitcher currentScreen={currentScreen} onScreenChange={handleScreenChange}>
+                    <MainMenu key={"main-menu"} onScreenChange={handleScreenChange}/>
+                    <MainMenu key={"about"} onScreenChange={handleScreenChange}/>
+                </ScreenSwitcher>                
             </div>
         </div> 
     );
--- src/components/MainMenuItem.tsx
+++ src/components/MainMenuItem.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image"
+import { FC } from "react";
 
 type MainMenuItemProps = {
     title: string;
     image: string;
     imageAlt: string;
+    screenName: string;
+    onScreenChange: (screen: string) => void;
 }
 
-export const MainMenuItem = ({title, image, imageAlt}: MainMenuItemProps) => {
+export const MainMenuItem:FC<MainMenuItemProps> = ({title, image, imageAlt, screenName, onScreenChange}) => {
     
     return (
-        <div className="main-menu-item">
+        <div className="main-menu-item" onClick={() => onScreenChange(screenName)}>
             <div className="main-menu-item__content">
                 <Image src={image} alt={imageAlt} width={300} height={300} />
                 <h2>{title}</h2>
