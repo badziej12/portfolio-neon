f063c0be5820d017c9891c3ec0671f61eae4de76
badziej12 <badziej12@gmail.com>
2025-02-17 20:15:17 +0100

    Fix main menu slider animation
    Changes made:
     - Animation in main menu is no longer delayed after full sequence

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -4,9 +4,11 @@ import Slider from "react-slick";
 import aboutItemImage from "../../public/assets/images/about-menu-item.png";
 import projectsItemImage from "../../public/assets/images/projects-menu-item.png";
 import contactItemImage from "../../public/assets/images/contact-menu-item.png";
+import { SetStateAction, ComponentState, useState } from "react";
 
 export const MainMenu = () => {
     const { isFirstLoad } = useFirstLoad();
+    const [activeIndex, setActiveIndex] = useState<number>(0);
 
     const menuItems = [
         {
@@ -35,18 +37,25 @@ export const MainMenu = () => {
         dots: false,
         infinite: true,
         speed: 500,
-        slidesToShow: 1,
+        slidesToShow: 3,
         slidesToScroll: 1,
         waitForAnimate: true,
+        beforeChange: (_: number, next: SetStateAction<number>) => setActiveIndex(next), // Aktualizujemy indeks PRZED zmianą slajdu
       };
     
     return (
         <div className={`main-menu ${isFirstLoad ? "main-menu--first-load" : ""}`}>
             <div className={`main-menu__content ${isFirstLoad ? "main-menu__content--first-load" : ""}`}>
                 <Slider {...settings}>
-                    {menuItems.map((item) => (
-                        <MainMenuItem key={item.title} title={item.title} image={item.image} imageAlt={item.imageAlt} screenName={item.screenName} />
-                    ))}
+                    {menuItems.map((item, index) => {
+                        const nextIndex = (activeIndex + 1) % menuItems.length;
+
+                        return (
+                            <div key={item.title} className={`slide ${index === nextIndex ? "active-slide" : ""}`}>
+                                <MainMenuItem title={item.title} image={item.image} imageAlt={item.imageAlt} screenName={item.screenName} />
+                            </div>
+                        );
+                    })}
                 </Slider>
                 <div className="main-menu__desktop">
                     {menuItemsDesktop.map((item) => (
