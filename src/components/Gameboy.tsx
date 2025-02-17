a648b9b2fe8b387e3cbfa564bb5d299ae45a6175
badziej12 <badziej12@gmail.com>
2025-02-17 18:09:50 +0100

    Fix static files serving
    Changes made:
     - Removed inserting relative src paths to next Image component.
       Used imports instead
     - Changed string types for src in components to StaticImageData

--- src/components/Gameboy.tsx
+++ src/components/Gameboy.tsx
@@ -1,6 +1,8 @@
 import { useScreen } from "@/context/ScreenContext";
 import { GameboyScreen } from "./GameboyScreen";
 import Image from "next/image";
+import gameboyScreenImg from "../../public/assets/images/gameboy/gameboy-screen.png";
+import gameboyDeviceImg from "../../public/assets/images/gameboy/gameboy-device.png";
 
 export const Gameboy = () => {
     const { currentScreen } = useScreen();
@@ -10,8 +12,8 @@ export const Gameboy = () => {
             <div className={"gameboy-component__device"}>
                 <div className={`gameboy-component__background ${currentScreen !== 'main-menu' ? 'darken' : ''}`}></div>
                 <div className="gameboy-component__gameboy-image-container">
-                    <Image className="gameboy-component__gameboy-device-image" src={"/assets/images/gameboy/gameboy-device.png"} alt="Gameboy device" width={3351} height={3351} />
-                    <Image className="gameboy-component__gameboy-screen-image" src={"/assets/images/gameboy/gameboy-screen.png"} alt="Gameboy screen" width={3351} height={3351} />
+                    <Image className="gameboy-component__gameboy-device-image" src={gameboyDeviceImg} alt="Gameboy device" width={3351} height={3351} />
+                    <Image className="gameboy-component__gameboy-screen-image" src={gameboyScreenImg} alt="Gameboy screen" width={3351} height={3351} />
                 </div>
             </div>
             <GameboyScreen />
