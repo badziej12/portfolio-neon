61ad9cc8e37bd3a9ab6ab20e497fad8b085f1b69
badziej12 <badziej12@gmail.com>
2025-02-21 19:12:02 +0100

    Display fixes
    Change made:
     - Viewoport height is now set to dynamically for mobile
     - Removed scrollable main menu
     - Decresaesd text glowing
     - ConsoleScreen is now loadable too

--- src/components/ConsoleScreen.tsx
+++ src/components/ConsoleScreen.tsx
@@ -1,6 +1,10 @@
 import { FC } from "react";
 import { ExitButton } from "./ExitButton";
 import { useLoading } from "@/context/LoadingContext";
+import cornerLeftImg from "../../public/assets/images/about/left-corner.png";
+import cornerUpRight from "../../public/assets/images/about/up-right-corner.png";
+import cornerDownRightImg from "../../public/assets/images/about/down-right-corner.png";
+import Image from "next/image";
 
 
 type ConsoleScreenProps = {
@@ -8,15 +12,17 @@ type ConsoleScreenProps = {
 }
 
 export const ConsoleScreen: FC<ConsoleScreenProps> = ({ children }) => {
-    const { isCompletedLoading } = useLoading();
+    const { isCompletedLoading, handleItemLoading } = useLoading();
+
+    
 
     return (
         <div style={{display: isCompletedLoading ? "flex" : "none"}} className="console-screen-wrapper">
             <ExitButton />
             <div className="console-screen">
-                <span className="console-screen__corner console-screen__corner--left"></span>
-                <span className="console-screen__corner console-screen__corner--up-right"></span>
-                <span className="console-screen__corner console-screen__corner--down-right"></span>
+                <Image className="console-screen__corner console-screen__corner--left" src={cornerLeftImg} width={147} height={122} alt={"Corner"} onLoadingComplete={handleItemLoading} />
+                <Image className="console-screen__corner console-screen__corner--up-right" src={cornerUpRight} width={95} height={125} alt={"Corner"} onLoadingComplete={handleItemLoading} />
+                <Image className="console-screen__corner console-screen__corner--down-right" src={cornerDownRightImg} width={172} height={112} alt={"Corner"} onLoadingComplete={handleItemLoading} />
                 <div className="console-screen__container">
                     <div className="console-screen__content">
                         {children}
